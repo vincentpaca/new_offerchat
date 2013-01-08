1e88f2b2cd63060a549d3a1a921bc35533f0391b
harvey enrile <harveyenrile@gmail.com>
2013-01-08 16:43:43 +0800

    updates added save response modal

--- app/assets/javascripts/zcommon.js
+++ app/assets/javascripts/zcommon.js
@@ -21,7 +21,13 @@ $(document).ready(function(){
     $(".chat-area input[type=text]").focus();
     $(".chat-area").addClass('active');
   
-
+$("#addResponseBtn").click(function(){
+  $("#addResponseModal").removeClass('hide');
+  $(".modal-backdrop").removeClass('hide');
+});
+$(".close").click(function(){
+  $(".modal,.modal-backdrop").addClass('hide');
+});
     
 $(function(){
   $('#more-info').click(function() {
