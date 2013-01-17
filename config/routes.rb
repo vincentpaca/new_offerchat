0d7ba4749fdfe25fe5117f88910e00145eed441d
rein <reinafiguracion@gmail.com>
2013-01-17 10:46:23 -0800

    Update rein

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ NewOfferchat::Application.routes.draw do
   get :chat_limit, :controller => :home, :action => :chat_limit
   get :widget_settings, :controller => :home, :action => :widget_settings
   get :users, :controller => :home, :action => :users 
-
   get :widget_settings_post_chat, :controller => :home, :action => :widget_settings_post_chat
   get :widget_settings_offline_form, :controller => :home, :action => :widget_settings_offline_form
   get :widget_settings_pre_chat, :controller => :home, :action => :widget_settings_pre_chat
