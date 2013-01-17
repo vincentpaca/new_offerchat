dfda0f01d7c0409855ed910d78c156e7d876185e
rein <reinafiguracion@gmail.com>
2013-01-16 17:59:25 -0800

    Updates -rein

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ NewOfferchat::Application.routes.draw do
   get :chat_timeout, :controller => :home, :action => :chat_timeout
   get :chat_limit, :controller => :home, :action => :chat_limit
   get :widget_settings, :controller => :home, :action => :widget_settings
+  get :users, :controller => :home, :action => :users 
+
   get :widget_settings_post_chat, :controller => :home, :action => :widget_settings_post_chat
   get :widget_settings_offline_form, :controller => :home, :action => :widget_settings_offline_form
   get :widget_settings_pre_chat, :controller => :home, :action => :widget_settings_pre_chat
