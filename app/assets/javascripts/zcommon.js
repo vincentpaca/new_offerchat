c148f32a00215c8154e5085bb08177d8840e8125
rein <reinafiguracion@gmail.com>
2013-01-22 09:51:30 -0800

    Update rein

--- app/assets/javascripts/zcommon.js
+++ app/assets/javascripts/zcommon.js
@@ -26,13 +26,14 @@ $("#addResponseBtn").click(function(){
   $("#addResponseModal").removeClass('hide');
   $(".modal-backdrop").removeClass('hide');
 });
-<<<<<<< HEAD
+
 $("#resetPasswordBtn").click(function(){
   $("#resetPasswordModal").removeClass('hide');
-=======
+  $(".modal-backdrop").removeClass('hide');
+});
+
 $("#transferChatBtn").click(function(){
   $("#transferChatModal").removeClass('hide');
->>>>>>> b67493b2d488b711f80280f562152fec38591503
   $(".modal-backdrop").removeClass('hide');
 });
 $("#addTriggerBtn").click(function(){
